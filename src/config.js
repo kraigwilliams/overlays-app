e4f365bca944cddf1856a3bbc0e89156d2ed55cc
kraigwilliams <krgwilliams@gmail.com>
2020-02-18 15:56:15 -0400

    heroku deployment address

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default{
-API_ENDPOINT: 'http://localhost:8000/api',
+// API_ENDPOINT: 'http://localhost:8000/api',
+API_ENDPOINT: 'https://kraig-overlays-server.herokuapp.com/api',
 TOKEN_KEY: 'overlays-client-auth-token'
     
 }
