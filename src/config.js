825dd11e6e26f24b1e302196f4ccccfbe0df96b9
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-02-20 03:40:16 -0400

    redep

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default{
 // API_ENDPOINT: 'http://localhost:8000/api',
-API_ENDPOINT: 'https://kraig-overlays-server.herokuapp.com/api' || 'http://localhost:8000',
+API_ENDPOINT: 'https://kraig-overlays-server.herokuapp.com/api' || 'http://localhost:8000/api',
 TOKEN_KEY: 'overlays-client-auth-token'
     
 }
