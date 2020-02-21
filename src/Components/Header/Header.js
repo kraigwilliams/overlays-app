d76716a66e1222e72048774b3488e5cf801a31d9
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-02-21 02:14:18 -0400

    edit readme.md

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
  import './header.css'
-import Nav from '../Nav/Nav'
+//import Nav from '../Nav/Nav'
 
 
 function Header(){
 return(
     <header>
     <Link to= "/"><h1 id="header">Overlays App</h1></Link>
-    <span id ="nav"><Nav /></span>
+    {/* <span id ="nav"><Nav /></span> */}
     </header>
 )
 
--- src/Components/NotesPage/NotesListPage.js
+++ src/Components/NotesPage/NotesListPage.js
@@ -1,57 +1,55 @@
-import React,{Component} from 'react'
+import React, { Component } from "react";
 //import Note from './Note';
-import {Link} from 'react-router-dom';
-import './notes.css';
-import config from '../../config'
-import TokenService from '../../services/token-service'
+import { Link } from "react-router-dom";
+import "./notes.css";
+import config from "../../config";
+import TokenService from "../../services/token-service";
 
+class NoteListPage extends Component {
+  componentDidMount() {
+    console.log("this.props.topic", this.props.topic);
+  }
 
-class NoteListPage extends Component{
-    componentDidMount(){
-        console.log("this.props.topic",this.props.topic)
-    }
-  
+  // when delete button is clicked the topic is deleted
+  handleDelete = event => {
+    event.preventDefault();
+    const topicId = this.props.topic.id;
+    fetch(`${config.API_ENDPOINT}/topics/${topicId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+        authorization: "bearer " + TokenService.getAuthToken()
+      }
+    }).then(() => this.componentDidMount());
+  };
 
-handleDelete= event=>{
-event.preventDefault();
-const topicId= this.props.topid.id
-fetch(`${config.API_ENDPOINT}/topic/${topicId}`,{
-method:"DELETE",
-headers: {
-  "content-type": "application/json",
-  authorization:"bearer " + TokenService.getAuthToken()
-}
-
-
-
-})
-}
-
-render(){
+  render() {
     //const topicId= this.props.match.params.TopicId
-return (
-    <div className ="all-topics">
-    
-
-     <Link  to={`/topics/${this.props.topic.id}`}><h2 className="topics-heading">{this.props.topic.topic_name}</h2></Link>
-      <p>
-        <a href={this.props.topic.topic_url}>{this.props.topic.topic_url}</a><br/>
-        {/* <label htmlFor="select-group">Share to Group:</label> */}
-        {/* <select id="select-group" value={this.state.groups}>
+    return (
+      <div className="all-topics">
+        <Link to={`/topics/${this.props.topic.id}`}>
+          <h2 className="topics-heading">{this.props.topic.topic_name}</h2>
+        </Link>
+        <p>
+          <a href={this.props.topic.topic_url}>{this.props.topic.topic_url}</a>
+          <br />
+          {/* <label htmlFor="select-group">Share to Group:</label> */}
+          {/* <select id="select-group" value={this.state.groups}>
           {this.state.groups.map(group=>{
             return <option>{group.user_groups}</option>
           })}
           
         </select> */}
-       <Link to={`/topics/${this.props.topic.id}`}>View Notes</Link><br/>
-       <Link to="/new-note">Add Note to Topic</Link><br/>
+          <Link to={`/topics/${this.props.topic.id}`}>View Notes</Link>
+          <br />
+          <Link to="/new-note">Add Note to Topic</Link>
+          <br />
 
-        <button onClick={this.handleDelete}>Delete</button> 
-      </p>
-    </div>
-  )
+          <button onClick={this.handleDelete}>Delete</button>
+        </p>
+      </div>
+    );
+  }
 }
 
-}
-
-export default NoteListPage
+export default NoteListPage;
