5ea0afffee1580c2dedae1ab44e7580c255e9e0a
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-02-28 17:47:32 -0500

    edit css

--- src/Components/NotesPage/AllNotes.js
+++ src/Components/NotesPage/AllNotes.js
@@ -1,84 +1,73 @@
-import React,{Component} from 'react';
-import config from '../../config'
-import TokenService from '../../services/token-service'
+import React, { Component } from "react";
+import config from "../../config";
+import TokenService from "../../services/token-service";
 //import Note from './Note'
-import {Link } from 'react-router-dom'
-import './notes.css'
+import { Link } from "react-router-dom";
+import "./notes.css";
 
+class AllNotes extends Component {
+  state = {
+    notes: []
+  };
 
-class AllNotes extends Component{
-    
-    state={
-        notes:[]
-    }
+  componentDidMount() {
+    document.title = "Topic Notes - Overlays";
+    const topicId = this.props.match.params.topicId;
 
-    componentDidMount(){
-        document.title="Topic Notes - Overlays"
-        const topicId= this.props.match.params.topicId
-        
+    fetch(`${config.API_ENDPOINT}/notes/bytopic/${topicId}`, {
+      headers: {
+        authorization: "bearer " + TokenService.getAuthToken()
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ notes: data });
+      });
+  }
 
-        fetch(`${config.API_ENDPOINT}/notes/${topicId}`,{
-            headers:{
-                authorization:"bearer " + TokenService.getAuthToken()
-              }
-        })
-        .then(res=> res.json())
-        .then(data=>{
-            
-            this.setState({notes:data})
-                      
-        }
-            )
+  deleteNote = event => {
+    event.preventDefault();
+console.log(event.currentTarget)
+    fetch(`${config.API_ENDPOINT}/notes/${event.currentTarget.value}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+        authorization: "bearer " + TokenService.getAuthToken()
+      }
+    }).then(this.props.history.push("/"));
+  };
 
-    }
+  render() {
+    //   <Note key={this.state.notes.id} note={this.state.notes} />
 
-    deleteNote= event=>{
-        event.preventDefault();
-        
-        
-        fetch(`${config.API_ENDPOINT}/notes/${event.target.value}`,{
-          method:"DELETE",
-          headers: {
-            "content-type": "application/json",
-            authorization: "bearer " + TokenService.getAuthToken()
-          }
-        
-        })
-        .then(this.props.history.push('/'))
-        }
+    return (
+      <>
+        <p>
+          <Link to="/new-note">Add New Note</Link>
+        </p>
 
-render(){
-//   <Note key={this.state.notes.id} note={this.state.notes} />
-  
-return(
-    <>
-
-    <p><Link to="/new-note">Add New Note</Link></p>
-
-
-    {this.state.notes.map(note=>{
-    
-   return(
-    
-   <div key={note.note_title} className="all-notes">
-        
-   <Link to="#"><h3><button name="delete-btn" value={note.id} onClick={this.deleteNote}><i className="far fa-trash-alt"></i>
-
-</button>{note.note_title}</h3></Link>
-   <p  >{note.note_contents}</p></div>)
-   }
-        )
-
-
-
-    }
-    </>
-    )
-
-
+        {this.state.notes.map(note => {
+          return (
+            <div key={note.note_title} className="all-notes">
+              <Link to="#">
+                <h3>
+                  <button
+                    name="delete-btn"
+                    value={note.id}
+                    onClick={this.deleteNote}
+                  >
+                    <i className="far fa-trash-alt"></i>
+                  </button>
+                  {note.note_title}
+                </h3>
+              </Link>
+              <p>{note.note_contents}</p>
+            </div>
+          );
+        })}
+      </>
+    );
+  }
 }
 
-
-  }
-  
-export default AllNotes
+export default AllNotes;
