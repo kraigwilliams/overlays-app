ed3c78e050b0cd781abb1152873ab17452642181
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-04-17 09:38:05 -0400

    finish tests

--- src/Components/Logout/Logout.test.js
+++ src/Components/Logout/Logout.test.js
@@ -1,20 +1,20 @@
 import React from 'react';
 //import { render } from '@testing-library/react';
-import Login from './Login'
+import Logout from './Logout'
 import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from  'react-dom';
 import renderer from 'react-test-renderer';
 
-describe('<Login/>',()=>{
+describe('<Logout/>',()=>{
   it ('is able to render',()=>{
     const div = document.createElement('div')
-    ReactDOM.render(<BrowserRouter><Login/></BrowserRouter>, div)
+    ReactDOM.render(<BrowserRouter><Logout/></BrowserRouter>, div)
     ReactDOM.unmountComponentAtNode(div)
   })
 
   it('renders the UI as expected', () => {
     const tree = renderer
-      .create(<BrowserRouter><Login/></BrowserRouter>)
+      .create(<BrowserRouter><Logout/></BrowserRouter>)
       .toJSON();
     expect(tree).toMatchSnapshot();  
     });
--- src/Components/Nav/Nav.test.js
+++ src/Components/Nav/Nav.test.js
@@ -1,20 +1,20 @@
 import React from 'react';
 //import { render } from '@testing-library/react';
-import Login from './Login'
+import Nav from './Nav'
 import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from  'react-dom';
 import renderer from 'react-test-renderer';
 
-describe('<Login/>',()=>{
+describe('<Nav/>',()=>{
   it ('is able to render',()=>{
     const div = document.createElement('div')
-    ReactDOM.render(<BrowserRouter><Login/></BrowserRouter>, div)
+    ReactDOM.render(<BrowserRouter><Nav/></BrowserRouter>, div)
     ReactDOM.unmountComponentAtNode(div)
   })
 
   it('renders the UI as expected', () => {
     const tree = renderer
-      .create(<BrowserRouter><Login/></BrowserRouter>)
+      .create(<BrowserRouter><Nav/></BrowserRouter>)
       .toJSON();
     expect(tree).toMatchSnapshot();  
     });
