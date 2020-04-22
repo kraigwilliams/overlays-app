5590d68757845118a2072abaa5e41002f4d37164
kraigyg <wiliamskrg@gmail.com>
2020-04-22 12:27:29 -0400

    test css

--- src/Components/Utils/Utils.js
+++ src/Components/Utils/Utils.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import { format as formatDate } from 'date-fns'
+
 import './Utils.css'
 
-export function NiceDate({ date, format='Do MMMM YYYY' }) {
-  return formatDate(date, format)
-}
 
 
 
